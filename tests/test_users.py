11c63d02380bcb7d6a969ed6140681a36e026297
Ersen Sarioglu <ersen.sarioglu@gmail.com>
2024-02-27 09:24:50 +0000

    test fixture setup

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,32 +1,5 @@
-from fastapi.testclient import TestClient
-import pytest
-from app.main import app
-from app import schemas, models
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from app.config import settings
-from app.database import get_db, Base
-
-SQLACHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
-
-engine = create_engine(SQLACHEMY_DATABASE_URL)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def override_get_db():
-    """Dependency for controlling db session"""
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-app.dependency_overrides[get_db] = override_get_db
-
-@pytest.fixture
-def client():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    yield TestClient(app)
+from app import schemas
+from tests.database import client,session
 
 def test_root(client):
     print("Root path")
