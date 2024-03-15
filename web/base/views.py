d421d9cd2043f90992750fce19e4403a2dbac036
Ersen Sarioglu <ersen.sarioglu@gmail.com>
2024-03-15 18:53:42 +0000

    room is also added to app template

--- web/base/views.py
+++ web/base/views.py
@@ -10,4 +10,4 @@ def home(request):
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
