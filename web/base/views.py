523c0f1e6465cd0ac9034c340d99025f58135de1
Ersen Sarioglu <ersen.sarioglu@gmail.com>
2024-03-15 18:49:54 +0000

    very simple jinja template example

--- web/base/views.py
+++ web/base/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Front end developers'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
 def room(request):
     return render(request, 'room.html')
