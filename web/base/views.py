28fa72a8776e8fc37c1e418e3a6993fbdca5adac
Ersen Sarioglu <ersen.sarioglu@gmail.com>
2024-03-17 11:55:50 +0000

    User update created

--- web/base/views.py
+++ web/base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from base.models import Room, Topic, Message
-from base.forms import RoomForm
+from base.forms import RoomForm, UserForm
 
 def loginPage(request):
     page = 'login'
@@ -148,4 +148,15 @@ def deleteMessage(request, pk):
         message.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj':message})
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+    return render(request, 'base/update-user.html', {'form': form})
     
